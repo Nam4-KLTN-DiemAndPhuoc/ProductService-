9ddfc849d2125320009669ef921f637c78dd6a4b
phuoc1707 <tdphuoc77@gmail.com>
2022-02-20 13:39:04 +0700

    update

--- src/main/java/com/example/productservice/common/Constants.java
+++ src/main/java/com/example/productservice/common/Constants.java
@@ -1,6 +1,6 @@
 package com.example.productservice.common;
 
 public class Constants {
-    public static  final String CATEGORY="http://localhost:8081/api/category";
-    public static  final String SUPPLIER="http://localhost:7070/api/supplier";
+    public static  final String CATEGORY="http://localhost:8081/api/category-service/category";
+    public static  final String SUPPLIER="http://localhost:7070/api/supplier-service/supplier";
 }
